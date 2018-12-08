b66d03c0075523f7ad4588f95b7b59c5a0a6172b
matteo-fisso <ma.addesso@gmail.com>
2018-12-08 16:31:50 +0100

    Creato exe, agiunti altri controlli, agggiunta funzionalità di poter visualizzare la cartella del file error

--- CheckCdrControl.py
+++ CheckCdrControl.py
@@ -18,31 +18,36 @@ class CheckCdrControl:
                 self.gsm(element)
                 if self.errGsm==True:
                     self.createLog(element)
-                    messagebox.showwarning(message="Check the file log in the folder \"Log Error In Cdr\"")
+                    # messagebox.showwarning(message="Check the file log in the folder \"Log Error In Cdr\"")
                     # error=messagebox.askyesno(title="Warning",message="There are errors in CDR. Do you want open the log folder?")
                     # if error:
-                    #     os.startfile("./Log Error In Cdr")
+                    #     os.startfile(".\\Log Error In Cdr")
                 continue
             elif "WCDMA"in element or "wcdma" in element or "3g" in element or "umts" in element or "UMTS" in element:
                 self.errGsm=False
                 self.wcdma(element)
                 if self.errGsm==True:
                     self.createLog(element)
-                    messagebox.showwarning(message="Check the file log in the folder \"Log Error In Cdr\"")
+                    # messagebox.showwarning(message="Check the file log in the folder \"Log Error In Cdr\"")
                     # error=messagebox.askyesno(title="Warning",message="There are errors in CDR. Do you want open the log folder?")
                     # if error:
-                    #     os.startfile("./Log Error In Cdr/")
+                    #     os.startfile(".\\Log Error In Cdr")
                 continue
             elif "LTE" in element or "lte" in element or "4g"in element:
                 self.errGsm=False
                 self.lte(element)
                 if self.errGsm==True:
                     self.createLog(element)
-                    messagebox.showwarning(message="Check the file log in the folder \"Log Error In Cdr\"")
+                    # messagebox.showwarning(message="Check the file log in the folder \"Log Error In Cdr\"")
                     # error=messagebox.askyesno(title="Warning",message="There are errors in CDR. Do you want open the log folder?")
                     # if error:
-                    #     os.startfile("./Log Error In Cdr/")
+                    #     os.startfile(".\\Log Error In Cdr")
                 continue
+        if self.errGsm==True:
+
+            error=messagebox.askyesno(title="Warning",message="There are errors in CDR. Do you want open the log folder?")
+            if error:
+                os.startfile(".\\Log Error In Cdr")
 
     def createLog(self, path):
         pathSplit=path.split("/")
@@ -78,7 +83,7 @@ class CheckCdrControl:
                 self.errGsm=True
                 if (element in listRecurrence)==False:
                     listRecurrence.append(element)
-                    self.listLineErrGsm.append(element+" is present more than once in column \"TG\"(column \"H\"))\n\n")
+                    self.listLineErrGsm.append(element+" is present more than once in column \"TG\"(column \"E\"))\n\n")
             pass
 
         for element in range(2,nTotCol+2):
@@ -89,16 +94,30 @@ class CheckCdrControl:
 
 
             if (doc["BTS"]["X"+str(element)].value!=None) or (doc["BTS"]["X"+str(element)].value!="0"):
-                if (doc["BTS"]["AB"+str(element)].value==None):
-                    self.errGsm=True
-                    self.listLineErrGsm.append("The value in "+"AB"+str(element)+" in sheet \"BTS\" must not be empty"+"\n\n")
-                if (doc["BTS"]["AC"+str(element)].value==None):
-                    self.errGsm=True
-                    self.listLineErrGsm.append("The value in "+"AC"+str(element)+" in sheet \"BTS\" must not be empty"+"\n\n")
 
                 nTrx=doc["BTS"]["X"+str(element)].value
                 startLett="F"
+                startLett1="C"
                 cellName=doc["BTS"]["C"+str(element)].value
+
+                if (doc["BTS"]["AB"+str(element)].value==None):
+                    self.errGsm=True
+                    self.listLineErrGsm.append("The value in "+"AB"+str(element)+" in sheet \"BTS\" must not be empty"+"\n\n")
+
+                if cellName[-2]=="G":
+                    if doc["BTS"]["D"+str(element)].value[-1]!="G":
+                        self.errGsm=True
+                        self.listLineErrGsm.append("The value in "+"D"+str(element)+" in sheet \"BTS\" must end with \"G\""+"\n\n")
+                elif cellName[-2]=="D":
+                    if doc["BTS"]["D"+str(element)].value[-1]!="D":
+                        self.errGsm=True
+                        self.listLineErrGsm.append("The value in "+"D"+str(element)+" in sheet \"BTS\" must end with \"D\""+"\n\n")
+
+                for e in range(0, nTrx):
+                    if doc["BTS"]["A"+startLett1+str(element)].value==None:
+                        self.errGsm=True
+                        self.listLineErrGsm.append("The cell "+"\""+"A"+startLett1+str(element)+"\" in sheet \"BTS\" "+"can not be empty.\n\n")
+                    startLett1=chr(ord(startLett1)+1)
 
                 for e in range(0,nTrx+1):
 
@@ -120,26 +139,18 @@ class CheckCdrControl:
 
                     startLett=chr(ord(startLett)+1)
 
-
-
             if doc["BTS"]["P"+str(element)].value==None:
                 self.errGsm=True
                 self.listLineErrGsm.append("The cell "+"\""+"P"+str(element)+"\" in sheet \"BTS\" "+"can not be empty.\n\n")
+
             if doc["BTS"]["R"+str(element)].value==None:
                 self.errGsm=True
                 self.listLineErrGsm.append("The cell "+"\""+"R"+str(element)+"\" in sheet \"BTS\" "+"can not be empty.\n\n")
 
 
+
         G2GValue=doc["ADJ G2G"]["A2"].value
         G2UValue=doc["ADJ G2U"]["A2"].value
-
-        nColG2G=countCol(doc,"ADJ G2G","A",2)
-        nColG2U=countCol(doc,"ADJ G2U","A",2)
-
-        elemInG2G=elemInCol(doc,"ADJ G2G","A",nColG2G)
-        elemInG2U=elemInCol(doc,"ADJ G2U","A",nColG2U)
-
-
         cont=2
         isDifferent=False
         while doc["ADJ G2U"]["A"+str(cont)].value!=None:
@@ -147,10 +158,33 @@ class CheckCdrControl:
                 isDifferent=True
                 break
             cont+=1
-
         if isDifferent:
             self.errGsm=True
             self.listLineErrGsm.append("The values in the column \"A\" of the sheet \"ADJ G2G\" mustn't be different from the values in the column \"A\" of the sheet \"ADJ G2U\"\n\n")
+
+        cont=2
+        isDifferentColBG2G=False
+        while doc["ADJ G2G"]["B"+str(cont)].value!=None:
+            if doc["ADJ G2G"]["A"+str(cont)].value!=doc["BTS"]["B2"]:
+                isDifferentColBG2G=True
+                break
+            cont+=1
+
+        if isDifferentColBG2G:
+            self.errGsm=True
+            self.listLineErrGsm.append("The values in the column \"B\" of the sheet \"ADJ G2G\" mustn't be different from the values in the column \"B\" of the sheet \"BTS\"\n\n")
+
+        cont=2
+        isDifferentColBG2U=False
+        while doc["ADJ G2U"]["B"+str(cont)].value!=None:
+            if doc["ADJ G2U"]["A"+str(cont)].value!=doc["BTS"]["B2"]:
+                isDifferentColBG2U=True
+                break
+            cont+=1
+
+        if isDifferentColBG2U:
+            self.errGsm=True
+            self.listLineErrGsm.append("The values in the column \"B\" of the sheet \"ADJ G2U\" mustn't be different from the values in the column \"B\" of the sheet \"BTS\"\n\n")
 
         doc.close()
         pass
